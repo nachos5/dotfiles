ffa5a14e7c78fd4843cee25d116b522396d4470e
nachos5 <gnorland93@gmail.com>
2023-02-28 21:19:20 +0000

    more fugitive mappings

--- .config/nvim/lua/plugins/glow.lua
+++ .config/nvim/lua/plugins/glow.lua
@@ -1,12 +1,6 @@
 -- https://github.com/charmbracelet/glow
 
-local nnoremap = require("keymap").nnoremap
-
 require("glow").setup({
   style = "dark",
   width = 120,
 })
-
-local default_opts = { silent = true }
-
-nnoremap("<leader>gl", "<cmd>Glow<CR>", default_opts)
--- .config/nvim/lua/plugins/vim-fugitive.lua
+++ .config/nvim/lua/plugins/vim-fugitive.lua
@@ -2,19 +2,24 @@ local nnoremap = require("keymap").nnoremap
 
 local shared_opts = { silent = true }
 
-nnoremap("<leader>gd", ":Gvdiffsplit<CR>", shared_opts)
+nnoremap("<leader>gs", ":vertical bo Git<CR>", shared_opts)
+nnoremap("<leader>gl", ":Gclog<cr>", shared_opts)
+nnoremap("<leader>gx", ":Gclog %<cr>", shared_opts)
+nnoremap("<leader>gc", ":Git commit<cr>", shared_opts)
+nnoremap("<leader>ga", ":Git add %<cr>", shared_opts)
+nnoremap("<leader>gd", ":Gvdiffsplit!<CR>", shared_opts)
 nnoremap("<leader>gm", ":Gvdiffsplit master<CR>", shared_opts)
 nnoremap("<leader>gb", ":Git blame<CR>", shared_opts)
-nnoremap("<leader>gs", ":vertical bo Git<CR>", shared_opts)
 nnoremap("<leader>gr", ":GBrowse<CR>", shared_opts)
+nnoremap("<leader>gp", ":Git push<CR>", shared_opts)
+nnoremap("<leader>gf", ":Git fetch<CR>", shared_opts)
+
+-- get from left
+nnoremap("<leader>g<left>", ":diffget //2<cr>", shared_opts)
+-- get from right
+nnoremap("<leader>g<right>", ":diffget //3<cr>", shared_opts)
 
 nnoremap("<leader>geh", ":Gedit<CR>", shared_opts)
 nnoremap("<leader>ge1", ":Gedit HEAD~1:%<CR>", shared_opts)
 nnoremap("<leader>ge2", ":Gedit HEAD~2:%<CR>", shared_opts)
 nnoremap("<leader>ge3", ":Gedit HEAD~3:%<CR>", shared_opts)
-nnoremap("<leader>ge4", ":Gedit HEAD~4:%<CR>", shared_opts)
-nnoremap("<leader>ge5", ":Gedit HEAD~5:%<CR>", shared_opts)
-nnoremap("<leader>ge6", ":Gedit HEAD~6:%<CR>", shared_opts)
-nnoremap("<leader>ge7", ":Gedit HEAD~7:%<CR>", shared_opts)
-nnoremap("<leader>ge8", ":Gedit HEAD~8:%<CR>", shared_opts)
-nnoremap("<leader>ge9", ":Gedit HEAD~9:%<CR>", shared_opts)
