fbc497bd4509ca114938d4dbb6c057ea2c7e0cc1
nachos5 <gnorland93@gmail.com>
2023-03-12 16:41:12 +0000

    tridactyl

--- install.sh
+++ install.sh
@@ -20,24 +20,25 @@ ln -sf "${DOTFILES}/.tmux.conf" ~/.tmux.conf
 ln -sf "${DOTFILES}/.xprofile" ~/.xprofile
 ln -sf "${DOTFILES}/.wezterm.lua" ~/.wezterm.lua
 ln -sf "${DOTFILES}/.local/bin/tmux-sessionizer" ~/.local/bin/tmux-sessionizer
+ln -sf "${DOTFILES}/.tridactylrc" ~/.tridactylrc
 
 if ! grep -q "if \[ -f ~/.bashrc \]; then" ~/.bash_profile; then
-  echo "if [ -f ~/.bashrc ]; then" >> ~/.bash_profile
-  echo "  . ~/.bashrc" >> ~/.bash_profile
-  echo "fi" >> ~/.bash_profile
+	echo "if [ -f ~/.bashrc ]; then" >>~/.bash_profile
+	echo "  . ~/.bashrc" >>~/.bash_profile
+	echo "fi" >>~/.bash_profile
 fi
 
 if ! grep -q "if \[ -f ~/.bash_aliases \]; then" ~/.bashrc; then
-  echo "if [ -f ~/.bash_aliases ]; then" >> ~/.bashrc
-  echo "  . ~/.bash_aliases" >> ~/.bashrc
-  echo "fi" >> ~/.bashrc
+	echo "if [ -f ~/.bash_aliases ]; then" >>~/.bashrc
+	echo "  . ~/.bash_aliases" >>~/.bashrc
+	echo "fi" >>~/.bashrc
 fi
 
 if ! grep -q "export EDITOR=nvim" ~/.bashrc; then
-  echo "export EDITOR=nvim" >> ~/.bashrc
+	echo "export EDITOR=nvim" >>~/.bashrc
 fi
 
 if ! grep -q "# set PS1" ~/.bashrc; then
-  echo "# set PS1" >> ~/.bashrc
-  echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >> ~/.bashrc
+	echo "# set PS1" >>~/.bashrc
+	echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '" >>~/.bashrc
 fi
