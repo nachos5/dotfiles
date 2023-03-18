0b636b44eeae760dd377da3f9f0eb44e2f053c67
nachos5 <gnorland93@gmail.com>
2023-03-18 00:15:40 +0000

    lua logging snippets

---
+++ utils/nvim/lua/snippets/lua.lua
@@ -0,0 +1,28 @@
+local ls = require("luasnip")
+local s = ls.snippet
+
+local my_lua_snippets = {
+  --logging
+  s("log", {
+    t("print("),
+    i(1),
+    t(")"),
+    i(0),
+  }),
+
+  -- logging with string label
+  s("logl", {
+    t("print("),
+    t('"'),
+    rep(1),
+    t('"'),
+    t(", "),
+    i(1),
+    t(")"),
+    i(0),
+  }),
+}
+
+ls.add_snippets("lua", my_lua_snippets)
+
+return my_lua_snippets
