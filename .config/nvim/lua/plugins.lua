2736bc0bf20bbeea015ee819809bfd94e66ee242
Guðmundur Óli Norland <gnorland93@gmail.com>
2025-01-10 00:09:45 +0100

    stuff (#7)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,13 +1,17 @@
+-- Bootstrap lazy.nvim.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,7 +23,6 @@ local config = {
     fallback = false,
   },
 }
--- function to require a plugin
 
 require("lazy").setup({
   -- core
@@ -36,7 +39,6 @@ require("lazy").setup({
 
   -- various / utils / uncategorized
   require("plugins/autopairs"),
-  "windwp/nvim-ts-autotag",
   require("plugins/tabout"),
   require("plugins/which_key"),
 
