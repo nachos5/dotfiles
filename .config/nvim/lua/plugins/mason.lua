3b198aa1e184b59e5a94a10a7205cdf1afea48d4
nachos5 <gnorland93@gmail.com>
2025-03-31 17:49:04 +0000

    lua ls mason

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -97,6 +97,33 @@ return {
           },
         })
       end,
+
+      ["lua_ls"] = function()
+        lspconfig.lua_ls.setup({
+          on_attach = require("lsp").on_attach,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }, -- Recognize 'vim' as a global.
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                  [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                  [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
+                  [vim.fn.expand("${3rd}/love2d/library")] = true,
+                },
+                maxPreload = 100000,
+                preloadFileSize = 10000,
+              },
+              telemetry = {
+                enable = false,
+              },
+            },
+          },
+        })
+      end,
     })
 
     -- rust setup
