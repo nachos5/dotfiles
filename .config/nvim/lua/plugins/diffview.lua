8b58624d394fa076e917855e44db4b399893d7a0
nachos5 <gnorland93@gmail.com>
2026-08-20 03:03:12 +0000

    diffview command

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -6,6 +6,18 @@ return {
     { "<leader>vc", ":DiffviewClose<CR>", desc = "Close Diffview" },
     { "<leader>vh", ":DiffviewFileHistory %<CR>", desc = "Current file history" },
     { "<leader>vm", ":DiffviewOpen master...HEAD<CR>", desc = "Diff against master" },
+    {
+      "<leader>vM",
+      function()
+        local merge_base = vim.trim(vim.fn.system("git merge-base master HEAD"))
+        if vim.v.shell_error ~= 0 then
+          vim.notify("git merge-base failed: " .. merge_base, vim.log.levels.ERROR)
+          return
+        end
+        vim.cmd("DiffviewOpen " .. merge_base)
+      end,
+      desc = "Diff against master incl. working changes",
+    },
   },
   opts = {
     default_args = {
