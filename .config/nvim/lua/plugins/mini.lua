ca1b77d69cefe9df7d3c179e8f6a37eebdc39e39
nachos5 <gnorland93@gmail.com>
2026-08-28 15:05:51 +0000

    ok

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -6,6 +6,70 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
+    local miniclue = require("mini.clue")
+    miniclue.setup({
+      triggers = {
+        { mode = { "n", "x" }, keys = "<Leader>" },
+        { mode = { "n", "x" }, keys = "<LocalLeader>" },
+        { mode = "n", keys = "[" },
+        { mode = "n", keys = "]" },
+        { mode = "i", keys = "<C-x>" },
+        { mode = { "n", "x" }, keys = "g" },
+        { mode = { "n", "x" }, keys = "'" },
+        { mode = { "n", "x" }, keys = "`" },
+        { mode = { "n", "x" }, keys = '"' },
+        { mode = { "i", "c" }, keys = "<C-r>" },
+        { mode = "n", keys = "<C-w>" },
+        { mode = { "n", "x" }, keys = "z" },
+      },
+      clues = {
+        miniclue.gen_clues.square_brackets(),
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+        { mode = "n", keys = "<Leader>x", desc = "+Trouble" },
+      },
+      window = { delay = 200 },
+    })
+
+    local animate = require("mini.animate")
+    animate.setup({
+      cursor = { enable = false },
+      scroll = {
+        timing = animate.gen_timing.linear({ duration = 120, unit = "total" }),
+      },
+      resize = { enable = false },
+      open = { enable = false },
+      close = { enable = false },
+    })
+
+    local function scroll_window(key, select_mode)
+      local count = math.max(1, math.floor(vim.api.nvim_win_get_height(0) * 0.1 + 0.5))
+      local keys = count .. vim.keycode(key)
+      if select_mode then
+        keys = vim.keycode("<C-o>") .. keys
+        vim.api.nvim_feedkeys(keys, "n", false)
+        return
+      end
+      vim.cmd.normal({ args = { keys }, bang = true })
+    end
+
+    vim.keymap.set({ "n", "x" }, "<C-y>", function()
+      scroll_window("<C-y>")
+    end, { desc = "Scroll window up 10%" })
+    vim.keymap.set("s", "<C-y>", function()
+      scroll_window("<C-y>", true)
+    end, { desc = "Scroll window up 10%" })
+    vim.keymap.set({ "n", "x" }, "<C-e>", function()
+      scroll_window("<C-e>")
+    end, { desc = "Scroll window down 10%" })
+    vim.keymap.set("s", "<C-e>", function()
+      scroll_window("<C-e>", true)
+    end, { desc = "Scroll window down 10%" })
+
     -- ga=
     require("mini.align").setup()
 
