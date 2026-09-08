be44c353bdb456c642bb8380863f176f51454d2c
nachos5 <gnorland93@gmail.com>
2026-09-08 21:06:07 +0000

    dont start spotify i3

--- .config/i3/start.sh
+++ .config/i3/start.sh
@@ -24,9 +24,10 @@ sleep 3
 i3-msg 'workspace 3; exec --no-startup-id firefox'
 sleep 3
 i3-msg 'workspace 4; exec --no-startup-id ~/utils/scripts/tmux_term.sh'
+# sleep 3
+# i3-msg 'workspace 7; exec --no-startup-id spotify'
+# sleep 4
 sleep 3
-i3-msg 'workspace 7; exec --no-startup-id spotify'
-sleep 4
 i3-msg 'workspace 1; exec --no-startup-id copyq'
 i3-msg 'workspace 1; exec --no-startup-id emote'
 
